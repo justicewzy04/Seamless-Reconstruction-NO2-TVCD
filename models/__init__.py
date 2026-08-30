84bed0470f6a45aaeb6020cbdbd812ea9bd2204b
wzy <148068002+justicewzy04@users.noreply.github.com>
2026-08-30 10:20:03 +0800

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Neural-network architectures used by UFill."""
-
-from .convlstm import ConvLSTM
-from .residual_unet import Unet as ResidualUNet
-from .segformer import SegFormer
-from .temporal_unet import Unet as TemporalUNet
-
-__all__ = ["ConvLSTM", "ResidualUNet", "SegFormer", "TemporalUNet"]
